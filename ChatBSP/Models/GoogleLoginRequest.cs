228f7738f8563b23c4ae8573e0399a6a886374f0
Benjamin Lee <prizefighter@gmail.com>
2018-06-18 14:06:25 -0700

    wip

---
+++ ChatBSP/Models/GoogleLoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChatBSP.Models
+{
+    public class GoogleLoginRequest
+    {
+        [Required]
+        public string GoogleToken { get; set; }
+    }
+}
---
+++ ChatBSP/Services/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using ChatBSP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBSP.Services
+{
+    interface IUsersService
+    {
+        bool GoogleLogin(GoogleLoginRequest model);
+    }
+}
